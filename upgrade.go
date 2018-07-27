c6912889c2b341b16f9c1fe3e467ae26308ed170
Vadim Bahmatovich <bahmatovich.v@gmail.com>
2018-07-27 18:36:28 +0300

    Panic fix

--- upgrade.go
+++ upgrade.go
@@ -6,6 +6,19 @@ import (
 	"path/filepath"
 )
 
+func checkUpgrade(db *sql.DB) error {
+	drows, err := GetDialect().dbVersionQuery(db)
+	if err != nil {
+		if err := createVersionTable(db); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	drows.Close()
+	return fmt.Errorf("table %s already exists", TableName())
+}
+
 // Upgrade migraion table
 func Upgrade(db *sql.DB, dir, note string) error {
 	var (
@@ -13,15 +26,9 @@ func Upgrade(db *sql.DB, dir, note string) error {
 		searchIndex int64
 	)
 
-	rows, err := GetDialect().dbVersionQuery(db)
-	if err != nil {
-		if err := createVersionTable(db); err != nil {
-			return err
-		}
-	} else {
-		return fmt.Errorf("table %s already exists", TableName())
+	if err := checkUpgrade(db); err != nil {
+		return err
 	}
-	rows.Close()
 
 	// collect all migrations
 	migrations, err := CollectMigrations(db, dir, minVersion, maxVersion)
@@ -29,7 +36,13 @@ func Upgrade(db *sql.DB, dir, note string) error {
 		return err
 	}
 
-	rows, err = db.Query(fmt.Sprintf("SELECT version_id, is_applied, tstamp FROM %s;", TableOldName()))
+	rows, err := db.Query(fmt.Sprintf("SELECT version_id, is_applied, tstamp FROM %s;", TableOldName()))
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	tx, err := db.Begin()
 	if err != nil {
 		return err
 	}
@@ -45,13 +58,12 @@ func Upgrade(db *sql.DB, dir, note string) error {
 		}
 
 		if row.VersionID == migrations[searchIndex].Version {
-			m := migrations[searchIndex]
-			if _, err := db.Exec(GetDialect().insertVersionSQL(), row.VersionID, filepath.Base(m.Source), note, row.IsApplied); err != nil {
+			if _, err := tx.Exec(GetDialect().insertVersionSQL(), row.VersionID, filepath.Base(migrations[searchIndex].Source), note, row.IsApplied); err != nil {
+				tx.Rollback()
 				return err
 			}
 		}
-
 	}
 
-	return nil
+	return tx.Commit()
 }
